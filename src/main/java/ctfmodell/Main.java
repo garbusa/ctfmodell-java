264aa0ae028d51e318b55cb27490938e845621c2
Nick Garbusa <nick.garbusa@uni-oldenburg.de>
2019-10-23 23:37:42 +0200

    - icons & tooltips added

--- src/main/java/ctfmodell/Main.java
+++ src/main/java/ctfmodell/Main.java
@@ -4,16 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+@SuppressWarnings("ConstantConditions")
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
-        primaryStage.setTitle("Capture The Flag");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+            primaryStage.setTitle("Capture The Flag");
+            primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("image/menu/police_with_flag.png")));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
     }
 
 
